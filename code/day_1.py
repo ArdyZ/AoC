db53d1966315492d198ad9d67461be9e2c1dffce
Ardy <a.zwanenburg@student.tudelft.nl>
2022-11-29 14:05:53 +0100

    Make template for AoC 2022

---
+++ code/day_1.py
@@ -0,0 +1,6 @@
+from helper import *
+
+# data = readNumbers("day_1/example.txt")
+data = readNumbers("./days/day_1/example.txt")
+
+print(data)
---
+++ code/helper.py
@@ -0,0 +1,23 @@
+import os
+
+def readNumbers(path):
+    # read as string
+    data = readStrings(path)
+    
+
+    
+    data = [int(i) for i in data]
+    return data
+
+def readStrings(path):
+    # open file
+    text_file = open(path)
+    # read whole file to a string
+    data = text_file.read()
+    
+    # close file
+    text_file.close()
+
+    # convert each row to an element
+    data = data.split("\n")
+    return list(data)
