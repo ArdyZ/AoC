3cbc620ebe8a90d6b86d1ec089f758c07ae595f4
Ardy <a.zwanenburg@student.tudelft.nl>
2022-12-01 06:23:11 +0100

    Solving day 1

--- code/day_1.py
+++ code/day_1.py
@@ -1,6 +1,17 @@
 from helper import *
 
-# data = readNumbers("day_1/example.txt")
-data = readNumbers("./days/day_1/example.txt")
+data = readStrings("./days/day_1/problem.txt")
+# data = readStrings("./days/day_1/example.txt")
 
-print(data)
+result = []
+current_sum = 0 
+
+for i in range(0, len(data)):
+    if (data[i] == ''):
+        result.append(current_sum)
+        current_sum = 0 
+    else:
+        current_sum += int(data[i])
+
+print('day1_1:', sorted(result)[-1])
+print('day1_2:', sum([sorted(result)[-1], sorted(result)[-2], sorted(result)[-3]]))
